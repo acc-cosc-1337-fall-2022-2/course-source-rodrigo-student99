46fa54522de7271fc97d80cd76d93c9c0545bcf4
rodrigo-student99 <rodrigo.silos@g.austincc.edu>
2022-10-02 00:17:50 +0000

    Finished HW with Test

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,29 @@
 //add include statements
-
+#include "dna.h"
+#include <algorithm> 
 //add function(s) code here
+
+int factorial(int num)
+{
+    int total = 1;
+    while (num > 0)
+    {
+        total = total * num;
+        num -= 1;
+    }
+    return total;
+}
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            std::swap(num1,num2);
+        }else {
+        num1 -= num2;
+    }
+    }   
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,11 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
+#include <string>
 //write using statements
-
-
+using std::cin;
+using std::cout;
+using std::string;
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
 or 2 for Get DNA Complement.  The program will prompt user for a 
@@ -12,5 +15,50 @@ user enters a y or Y.
 */
 int main() 
 {
+	bool is_over = false;
+	do
+	{
+		int option;
+		cout<<"\n\n1-Factorial"<<"\n";
+		cout<<"2-Greatest Common Divisor"<<"\n";
+		cout<<"3-Exit"<<"\n";
+		cout<<"Please type in your choice here:";
+		cin >> option;
+
+		if (option == 1)
+		{
+			int para;
+			int result;
+			cout<<"\nWhat number would you like to find factorial of? Type here:";
+			cin>>para;
+			result = factorial(para);
+			cout<<"\nThe factorial result of number you typed was "<<result;
+
+		} else if (option == 2){
+			int para1;
+			int para2;
+			int result;
+			cout<<"\nWhat is the first number to find gcd of? Type here:";
+			cin>>para1;
+			cout<<"\nWhat is the second number to find gcd of? Type here:";
+			cin>>para2;
+			result = gcd(para1,para2);
+			cout<<"\nThe result for gcd was "<<result;
+		} else{
+			string letter;
+			cout<<"Are you sure you want to quit? y/n?";
+			cin>>letter;
+
+			if (letter == "y"){
+				is_over = true;
+			}
+		}
+		
+		
+	}while (is_over == false);
+	
+	
+	
+	
 	return 0;
 }
