b364486ead8de2ba16a2df553d4055bb4b3556f1
rodrigo-student99 <rodrigo.silos@g.austincc.edu>
2022-10-07 00:43:08 +0000

    Finished functions with Tests and menu in main

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,57 @@
 //add include statements
+#include "func.h"
+#include <vector>
+
+using std::vector;
+
 
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double total = dna.length();
+    double acc = 0;
+    for(auto ch: dna)
+    {
+        if (ch == 'C' || ch == 'G')
+        {
+            acc += 1;
+        }
+    }
+    auto final_val = acc / total;
+    return final_val;
+}
+
+string reverse_string(string dna)
+{
+    string reverse;
+    for (int i = dna.size() - 1; i >= 0; i--)
+    {
+        reverse.push_back(dna[i]);
+    }
+
+    return reverse;
+}
+
+string get_dna_complement(string dna)
+{
+    string reverse_complement;
+    for (int i = dna.size() - 1; i >= 0; i--)
+    {
+        switch(dna[i])
+        {
+            case 'A' :
+                reverse_complement.push_back('T');
+                break;
+            case 'T' :
+                reverse_complement.push_back('A');
+                break;
+            case 'C' :
+                reverse_complement.push_back('G');
+                break;
+            case 'G' :
+                reverse_complement.push_back('C');
+                break;
+        }
+    }
+    return reverse_complement;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -7,8 +7,50 @@ and display them to screen.
 Program continues until user decides to exit.
 
 */
+#include "func.h"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+
 int main() 
 {
+	cout<<"\nHello user! Please select an option from menu";
+	cout<<"\n1- Get GC Content";
+	cout<<"\n2- Get DNA Complement";
+	cout<<"\n3- Exit";
+	cout<<"\nPlease type in your choice here: ";
+	char choice;
+	cin >> choice;
+
+	switch(choice)
+	{
+		{
+		case '1':
+			string strand;
+			double result;
+			cout<<"\nType in the strand you want the GC content of here: ";
+			cin >> strand;
+			result = get_gc_content(strand);
+			cout<<"\nThe GC content is "<<result<<"\n";
+			break;
+		}
+		{
+		case '2':
+			string strand;
+			string result;
+			cout<<"\nType in the strand you want the complenent of here: ";
+			cin >> strand;
+			result = get_dna_complement(strand);
+			cout<<"\nThe DNA complement is "<<result<<"\n";
+			break;
+		}
+		default:
+			cout<<"\nYou have chosen to exit the menu\n";
+			break;
+	}
+
 
 	return 0;
 }
